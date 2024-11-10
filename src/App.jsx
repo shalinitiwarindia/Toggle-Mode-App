d7f2ad3850d94876fcf00ccd7570db7499d13d92
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-11-10 20:23:00 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import React from "react";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  
+  return (
+    <div>
+    <Header />
+     <Footer />
+    </div>
+  );
+};
+
+export default App;
